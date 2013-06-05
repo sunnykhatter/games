2809833090eac795994b6f3af09e859ed2ab6cee
Eric Szeto <eszeto@gmail.com>
2013-06-05 09:30:32 -0400

    finished tictactoe ha

--- tictactoe.rb
+++ tictactoe.rb
@@ -4,7 +4,7 @@ attr_accessor :board, :turn, :player, :spots
 
 	def initialize()
 	@board = drawboard()
-	@spots = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	@spots = "123456789"
 	@turn = 0
 	@wincase = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 	@player = "X"
@@ -15,18 +15,18 @@ attr_accessor :board, :turn, :player, :spots
 	end
 
 	def alreadytaken(number)
-		!@spots.include? number
+		!@spots.include? number.to_s
 	end	
 
 	def replacespot(number)
 		@board[number.to_s] = @player
-		@spots[number-1] = @player
+		@spots[number.to_s] = @player
 		@turn += 1
 		
 	end
 
 	def place(number)
-		if @spots.include? number 
+		if @spots.include? number.to_s 
 			replacespot(number)
 		end
 	end
@@ -39,24 +39,31 @@ attr_accessor :board, :turn, :player, :spots
 		end
 	end
 
-	def checkwinner()
+	def checkwinner(player)
 		outer = 0
-		inner = 0
-		while outer <= @wincase.size do
-			line = 0
-			while inner < 3 do
-				
-				line += 1 if @spots[@wincase[outer][inner]] == @player
-				inner += 1
+		# inner = 0
+		@wincase.each do |outer|
+			towin = 0
+			outer.each do |winnum|
+				towin += 1 if @spots[winnum-1] == player
 			end
-			return true if line >= 3
-			outer += 1
+			return true if towin >= 3
 		end
+		# while outer <= 8 do
+		# 	3towin = 0
+		# 	while inner <= 3 do
+				
+		# 		3towin += 1 if @spots[@wincase[outer][inner]-1] == @player
+		# 		inner += 1
+		# 	end
+		# 	return true if 3towin >= 3
+		# 	outer += 1
+		# end
 		whosturn(@turn)
 	end
 
 	def win?
-		true if checkwinner() == true
+		true if checkwinner(@player) == true
 	end
 
 	def tie?
